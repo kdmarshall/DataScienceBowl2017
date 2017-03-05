60ca561bf2d7ea836f19d2faa274027b40f79965
Joshua Staker <Peace@Joshuas-MacBook-Pro-2.local>
2017-03-04 19:36:08 -0800

    Started preprocessing script with watershed method

--- scripts/preprocessing/process_data.py
+++ scripts/preprocessing/process_data.py
@@ -1,47 +1,292 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import dicom
-import numpy as np
-import pandas as pd
-from PIL import Image
-import cv2
+import os
+import scipy.ndimage as ndimage
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
-HM_SLICES = 20
+from skimage import measure, morphology, segmentation
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-# TODO: TF flags should be changed to args
-tf.app.flags.DEFINE_string("stage1",'','Full training examples set.')
-tf.app.flags.DEFINE_string("samples",'','Sample training examples set.')
-tf.app.flags.DEFINE_string("labels",'','Full training labels set.')
 
-if FLAGS.samples:
-    sample_data = get_sample_data(FLAGS.samples,FLAGS.labels)
-elif FLAGS.stage1:
-    print("No function to handle stage1 data yet.")
+def prepare_filepaths(dataset_path):
+    paths = []
+    for root, dirs, files in os.walk(dataset_path):
+        path = root.split('/')
+        for file in files:
+            if '.dcm' == file[-4:]:
+                paths.append(root+'/'+file)
+    return paths
 
-def get_sample_data(examples_path,labels_path):
-	# Not yet working!
-	# Still playing with data reformating
-	patients = [fname for fname in os.listdir(examples_path) if fname != '.DS_Store']
-	labels_df = pd.read_csv(labels_path, index_col='id')
-	for patient in patients:
-		try:
-			label = labels_df.get_value(str(patient), 'cancer')
-		except:
-			print("Patient {} not found".format(patient))
-			continue
-		file_path = os.path.join(examples_path,patient)
-		slices = [dicom.read_file(os.path.join(file_path,fname))
-					for fname in os.listdir(file_path)
-						if fname != '.DS_Store']
-		slices.sort(key=lambda x: int(x.ImagePositionPatient[2]))
-		slices = [cv2.resize(np.array(each_slice.pixel_array),(IMAGE_WIDTH,IMAGE_HEIGHT))
-					for each_slice in slices]
-		new_slices = []
-		chunk_sizes = int(math.ceil(len(slices) / HM_SLICES))
-		print(chunk_sizes)
-		for slice_chunk in chunks(slices,chunk_sizes):
-			slice_chunk = list(map(mean, zip(*slice_chunk)))
-			new_slices.append(slice_chunk)
+# Some constants 
+INPUT_FOLDER = '/Users/Peace/Desktop/Joshs/stage1/'
+patients = os.listdir(INPUT_FOLDER)
+patients.sort()
 
-		print(len(slices), len(new_slices))
-		break
 
-# TODO: Save out dataset (h5)
+
+# Load the scans in given folder path
+def load_scan(path):
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key = lambda x: int(x.InstanceNumber))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+        
+    for s in slices:
+        s.SliceThickness = slice_thickness
+        
+    return slices
+
+def get_pixels_hu(scans):
+    image = np.stack([s.pixel_array for s in scans])
+    # Convert to int16 (from sometimes int16), 
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+    
+    # Convert to Hounsfield units (HU)
+    intercept = scans[0].RescaleIntercept
+    slope = scans[0].RescaleSlope
+    
+    if slope != 1:
+        image = slope * image.astype(np.float64)
+        image = image.astype(np.int16)
+        
+    image += np.int16(intercept)
+    
+    return np.array(image, dtype=np.int16)
+
+# Some of the starting Code is taken from ArnavJain, since it's more readable then my own
+def generate_markers(image):
+    #Creation of the internal Marker
+    marker_internal = image < -400
+    marker_internal = segmentation.clear_border(marker_internal)
+    marker_internal_labels = measure.label(marker_internal)
+    areas = [r.area for r in measure.regionprops(marker_internal_labels)]
+    areas.sort()
+    if len(areas) > 2:
+        for region in measure.regionprops(marker_internal_labels):
+            if region.area < areas[-2]:
+                for coordinates in region.coords:                
+                       marker_internal_labels[coordinates[0], coordinates[1]] = 0
+    marker_internal = marker_internal_labels > 0
+    #Creation of the external Marker
+    external_a = ndimage.binary_dilation(marker_internal, iterations=10)
+    external_b = ndimage.binary_dilation(marker_internal, iterations=55)
+    marker_external = external_b ^ external_a
+    #Creation of the Watershed Marker matrix
+    marker_watershed = np.zeros((512, 512), dtype=np.int)
+    marker_watershed += marker_internal * 255
+    marker_watershed += marker_external * 128
+    
+    return marker_internal, marker_external, marker_watershed
+
+def seperate_lungs(image):
+    #Creation of the markers as shown above:
+    marker_internal, marker_external, marker_watershed = generate_markers(image)
+    
+    #Creation of the Sobel-Gradient
+    sobel_filtered_dx = ndimage.sobel(image, 1)
+    sobel_filtered_dy = ndimage.sobel(image, 0)
+    sobel_gradient = np.hypot(sobel_filtered_dx, sobel_filtered_dy)
+    sobel_gradient *= 255.0 / np.max(sobel_gradient)
+    
+    #Watershed algorithm
+    watershed = morphology.watershed(sobel_gradient, marker_watershed)
+    
+    #Reducing the image created by the Watershed algorithm to its outline
+    outline = ndimage.morphological_gradient(watershed, size=(3,3))
+    outline = outline.astype(bool)
+    
+    #Performing Black-Tophat Morphology for reinclusion
+    #Creation of the disk-kernel and increasing its size a bit
+    blackhat_struct = [[0, 0, 1, 1, 1, 0, 0],
+                       [0, 1, 1, 1, 1, 1, 0],
+                       [1, 1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1, 1],
+                       [0, 1, 1, 1, 1, 1, 0],
+                       [0, 0, 1, 1, 1, 0, 0]]
+    blackhat_struct = ndimage.iterate_structure(blackhat_struct, 8)
+    #Perform the Black-Hat
+    outline += ndimage.black_tophat(outline, structure=blackhat_struct)
+    
+    #Use the internal marker and the Outline that was just created to generate the lungfilter
+    lungfilter = np.bitwise_or(marker_internal, outline)
+    #Close holes in the lungfilter
+    #fill_holes is not used here, since in some slices the heart would be reincluded by accident
+    lungfilter = ndimage.morphology.binary_closing(lungfilter, structure=np.ones((5,5)), iterations=3)
+    
+    #Apply the lungfilter (note the filtered areas being assigned -2000 HU)
+    segmented = np.where(lungfilter == 1, image, -2000*np.ones((512, 512)))
+    
+    return segmented, lungfilter, outline, watershed, sobel_gradient, marker_internal, marker_external, marker_watershed
+
+test_patient_scans = load_scan(INPUT_FOLDER + patients[8])
+test_patient_images = get_pixels_hu(test_patient_scans)
+
+
+test_segmented, _, _, _, _, _, _, _ = seperate_lungs(test_patient_images[65])
+test_segmented[test_segmented == -2000] = 0
+print(np.max(test_segmented))
+print(np.min(test_segmented))
+
+sdfsdf
+
+
+from skimage import measure, morphology
+#from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+# Some constants 
+INPUT_FOLDER = '/Users/Peace/Downloads/sample_images/'
+patients = os.listdir(INPUT_FOLDER)
+patients.sort()
+
+# Load the scans in given folder path
+def load_scan(path):
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key = lambda x: int(x.InstanceNumber))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+        
+    for s in slices:
+        s.SliceThickness = slice_thickness
+        
+    return slices
+
+def get_pixels_hu(scans):
+    image = np.stack([s.pixel_array for s in scans])
+    # Convert to int16 (from sometimes int16), 
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+    
+    # Convert to Hounsfield units (HU)
+    intercept = scans[0].RescaleIntercept
+    slope = scans[0].RescaleSlope
+    
+    if slope != 1:
+        image = slope * image.astype(np.float64)
+        image = image.astype(np.int16)
+        
+    image += np.int16(intercept)
+    
+    return np.array(image, dtype=np.int16)
+
+def resample(image, scan, new_spacing=[1,1,1]):
+    # Determine current pixel spacing
+    spacing = map(float, ([scan[0].SliceThickness] + scan[0].PixelSpacing))
+    spacing = np.array(list(spacing))
+
+    resize_factor = spacing / new_spacing
+    new_real_shape = image.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / image.shape
+    new_spacing = spacing / real_resize_factor
+    
+    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
+    
+    return image, new_spacing
+
+
+
+def largest_label_volume(im, bg=-1):
+    vals, counts = np.unique(im, return_counts=True)
+
+    counts = counts[vals != bg]
+    vals = vals[vals != bg]
+
+    if len(counts) > 0:
+        return vals[np.argmax(counts)]
+    else:
+        return None
+
+def segment_lung_mask(image, fill_lung_structures=True):
+    
+    # not actually binary, but 1 and 2. 
+    # 0 is treated as background, which we do not want
+    binary_image = np.array(image > -320, dtype=np.int8)+1
+    labels = measure.label(binary_image)
+    
+    # Pick the pixel in the very corner to determine which label is air.
+    #   Improvement: Pick multiple background labels from around the patient
+    #   More resistant to "trays" on which the patient lays cutting the air 
+    #   around the person in half
+    background_label = labels[0,0,0]
+    
+    #Fill the air around the person
+    binary_image[background_label == labels] = 2
+    
+    
+    # Method of filling the lung structures (that is superior to something like 
+    # morphological closing)
+    if fill_lung_structures:
+        # For every slice we determine the largest solid structure
+        for i, axial_slice in enumerate(binary_image):
+            axial_slice = axial_slice - 1
+            labeling = measure.label(axial_slice)
+            l_max = largest_label_volume(labeling, bg=0)
+            
+            if l_max is not None: #This slice contains some lung
+                binary_image[i][labeling != l_max] = 1
+
+    
+    binary_image -= 1 #Make the image actual binary
+    binary_image = 1-binary_image # Invert it, lungs are now 1
+    
+    # Remove other air pockets insided body
+    labels = measure.label(binary_image, background=0)
+    l_max = largest_label_volume(labels, bg=0)
+    if l_max is not None: # There are air pockets
+        binary_image[labels != l_max] = 0
+ 
+    return binary_image
+
+first_patient = load_scan(INPUT_FOLDER + patients[0])
+first_patient_pixels = get_pixels_hu(first_patient)
+
+print(first_patient_pixels.shape)
+Image.fromarray(first_patient_pixels[64
+]).save('/Users/Peace/Desktop/example0.png')
+sdfsf
+
+pix_resampled, spacing = resample(first_patient_pixels, first_patient, [.1,.1,.1])
+print("Shape before resampling\t", first_patient_pixels.shape)
+print("Shape after resampling\t", pix_resampled.shape)
+
+segmented_lungs = segment_lung_mask(pix_resampled, False)
+segmented_lungs_fill = segment_lung_mask(pix_resampled, True)
+
+
+
+MIN_BOUND = -1000.0
+MAX_BOUND = 400.0
+    
+def normalize(image):
+    image = (image - MIN_BOUND) / (MAX_BOUND - MIN_BOUND)
+    image[image>1] = 1.
+    image[image<0] = 0.
+    return image
+
+PIXEL_MEAN = 0.25
+
+def zero_center(image):
+    image = image - PIXEL_MEAN
+    return image
+
+
+
+
+
