f3c6e326f3ce63fd0b878031c17509076872dd69
Robert Kramer <kramer102@gmail.com>
2017-03-25 18:20:04 -0700

    adding robert second model with dropout

--- models.py
+++ models.py
@@ -85,15 +85,16 @@ def baseline_model_robert(input):
     """
     
     h_conv1 = convolve(input, 1, 4, 'conv1')
-    h_conv2 = convolve(h_conv1, 4, 4, 'conv2')
-    h_conv3 = convolve(h_conv2, 4, 4, 'conv3')
-    h_conv4 = convolve(h_conv3, 4, 4, 'conv4')
-    h_conv5 = convolve(h_conv4, 4, 4, 'conv5')
-    h_conv6 = convolve(h_conv5, 4, 4, 'conv6')
+    h_conv2 = convolve(h_conv1, 4, 8, 'conv2')
+    h_conv3 = convolve(h_conv2, 8, 16, 'conv3')
+    h_conv4 = convolve(h_conv3, 16, 32, 'conv4')
+    h_conv5 = convolve(h_conv4, 32, 64, 'conv5')
+    h_conv6 = convolve(h_conv5, 64, 64, 'conv6')
     
     print("Transfer layer shape: {}".format(h_conv6.get_shape()))
     
     h_fc1 = fc_layer(h_conv6, 32, 'fc1')
+    h_fc1 = tf.nn.dropout(h_fc1, .81)
     output_logits = fc_layer(h_fc1, 1, 'fc2', activate=False)
         
     return output_logits
