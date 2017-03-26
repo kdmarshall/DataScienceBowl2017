b0f648f3550ef735e6a047a4e942a540fed13683
jstaker7 <jstaker7@gmail.com>
2017-03-25 19:55:31 -0700

    Added new model config

--- models.py
+++ models.py
@@ -126,3 +126,33 @@ def larger_first_model(input):
         
     return output_logits
 
+
+def balanced(input):
+    """
+    Run through the feed forward
+    3D Convolution Neural Network.
+
+    Layers:
+        3D ConvNet Layers w/ PRelu followed by 3D Maxpool
+
+        Fully Connected Layers w/ PRelu
+
+        FC Output Layer that provides the single logits unit
+    """
+    h_conv1 = convolve(input, 1, 8, 'conv1')
+    h_conv2 = convolve(h_conv1, 8, 16, 'conv2')
+    h_conv3 = convolve(h_conv2, 16, 16, 'conv3')
+    h_conv4 = convolve(h_conv3, 16, 32, 'conv4')
+    h_conv5 = convolve(h_conv4, 32, 64, 'conv5')
+    h_conv6 = convolve(h_conv5, 64, 128, 'conv6')
+    
+    print("Transfer layer shape: {}".format(h_conv6.get_shape()))
+    
+    h_fc1 = fc_layer(h_conv6, 128, 'fc1')
+    h_fc2 = fc_layer(h_conv6, 32, 'fc2')
+    output_logits = fc_layer(h_fc2, 1, 'fc3', activate=False)
+    
+    print(output_logits.get_shape())
+        
+    return output_logits
+
